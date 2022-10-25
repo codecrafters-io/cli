0c5a373c0a76ac47a327b958b9b5e2c0a689d88c
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-10-26 00:16:28 +0100

    minimal go-git usage

--- internal/commands/test.go
+++ internal/commands/test.go
@@ -1,8 +1,34 @@
 package commands
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
+	"os"
+)
 
 func TestCommand() int {
-	fmt.Println("Tests!")
+	dir, err := os.Getwd()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to fetch current working directory: %s", err)
+		return 1
+	}
+
+	repository, err := git.PlainOpen(dir)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to read git repository: %s", err)
+		return 1
+	}
+
+	branches, err := repository.Branches()
+	if err != nil {
+		panic(err)
+	}
+
+	branches.ForEach(func(branch *plumbing.Reference) error {
+		fmt.Println(branch.Name())
+		return nil
+	})
+
 	return 0
 }
