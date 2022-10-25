8e877872d3cb908c63bc79e16b137bee5eb0deec
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-10-26 00:25:26 +0100

    detect .git from parent directories

--- internal/commands/test.go
+++ internal/commands/test.go
@@ -3,7 +3,6 @@ package commands
 import (
 	"fmt"
 	"github.com/go-git/go-git/v5"
-	"github.com/go-git/go-git/v5/plumbing"
 	"os"
 )
 
@@ -14,21 +13,24 @@ func TestCommand() int {
 		return 1
 	}
 
-	repository, err := git.PlainOpen(dir)
+	repository, err := git.PlainOpenWithOptions(dir, &git.PlainOpenOptions{DetectDotGit: true})
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "failed to read git repository: %s", err)
 		return 1
 	}
 
-	branches, err := repository.Branches()
+	worktree, err := repository.Worktree()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to read worktree: %s", err)
+		return 1
+	}
+
+	status, err := worktree.Status()
 	if err != nil {
 		panic(err)
 	}
 
-	branches.ForEach(func(branch *plumbing.Reference) error {
-		fmt.Println(branch.Name())
-		return nil
-	})
+	fmt.Println(status)
 
 	return 0
 }
