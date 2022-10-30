2ce3948df27043cc45be1796790f9570fd9b1b79
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-10-30 15:24:32 +0000

    update ldflags usage

--- internal/utils/version.go
+++ internal/utils/version.go
@@ -2,9 +2,9 @@ package utils
 
 import "fmt"
 
-var version string = "0"
-var commit string = "unknown"
+var Version string = "0"
+var Commit string = "unknown"
 
 func VersionString() string {
-	return fmt.Sprintf("v%s-%s", version, commit[:7])
+	return fmt.Sprintf("v%s-%s", Version, Commit[:7])
 }
